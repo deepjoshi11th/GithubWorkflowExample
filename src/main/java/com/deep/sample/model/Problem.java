1d1481e6265e4c60e280e17c135de3352e1ea487
ishitakeshawani <keshwaniishita054@gmail.com>
2022-01-10 23:35:30 +0530

    implemented suggestions

--- src/main/java/com/deep/sample/model/Problem.java
+++ src/main/java/com/deep/sample/model/Problem.java
@@ -4,57 +4,45 @@ package com.deep.sample.model;
 public class Problem {
     private String title;
     private String descriptionValue;
-
     /**
      * it is constructor here.
      */
-
     public Problem() {
     }
-
     /**
      * problem is defined here.
      * @param title
      * @param descriptionValue
      */
-
     public Problem(String title, String descriptionValue) {
         this.title = title;
         this.descriptionValue = descriptionValue;
     }
-
     /**
      * this is  gettitle method.
      * @return it is returning the value
      */
-
     public String getTitle() {
         return title;
     }
-
     /**
      * setTitle declared here.
      * @param title
      */
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     /**
      * getDescriptionValue here.
      * @return returning the value.
      */
-
     public String getDescriptionValue() {
         return descriptionValue;
     }
-
     /**
      * it is comment.
      * @param descriptionValue
      */
-
     public void setDescriptionValue(String descriptionValue) {
         this.descriptionValue = descriptionValue;
     }
